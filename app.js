6390db08dedede4296a932488f662f8d24285061
seanlanders92 <seanlanders92@gmail.com>
2020-01-10 15:11:29 -0800

    fixed typo

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 var total = a * b;
-return [total, 'The sum of ' + a + ' and ' + b + ' is ' + total + '.']
+return [total, 'The product of ' + a + ' and ' + b + ' is ' + total + '.'];
 }
 
 //Here is the test for multiply(); uncomment it to run it
